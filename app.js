72e0b5d25be29f1f8b03f599f8ae8a29cab2377c
Damien White <damienkwhite@gmail.com>
2023-10-31 21:47:03 +0000

    added api call, browser is caching every request after first call, still researching ways to stop it from happening

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-async function advice() {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const advice = await response.json();
-    console.log(advice);
-  }
+const adviceText = document.querySelector("p");
+const adviceId = document.querySelector("span")
+const button = document.querySelector("#iconBackground")
+
+axios.default.headers = {
+  'Cache-Control': 'no-cache',
+  'Pragma': 'no-cache',
+  'Expires': '0',
+  'Access-Control-Allow-Origin': '*',
+};
+
+
+button.addEventListener("click", function (event){
+  event.preventDefault(); 
+axios
+  .get("https://api.adviceslip.com/advice")
+  .then((response) => 
+  {
+    console.log(response.data.slip.advice);
+    adviceText.innerHTML = response.data.slip.advice;
+    adviceId.innerHTML = response.data.slip.id;
+  })
+  
+  .catch(e => {
+    console.log("ERROR!:", e.message)
+  })
+});
+
