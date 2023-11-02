4970b3147a8cb78d5b903f08207d036084279f0d
Damien White <damienkwhite@gmail.com>
2023-11-02 08:31:00 +0000

    project completed

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ const adviceText = document.querySelector("p");
 const adviceId = document.querySelector("span")
 const button = document.querySelector("#iconBackground")
 
-axios.default.headers = {
-  'Cache-Control': 'no-cache',
-  'Pragma': 'no-cache',
-  'Expires': '0',
-  'Access-Control-Allow-Origin': '*',
-};
-
-
-button.addEventListener("click", function (event){
-  event.preventDefault(); 
-axios
-  .get("https://api.adviceslip.com/advice")
-  .then((response) => 
-  {
-    console.log(response.data.slip.advice);
-    adviceText.innerHTML = response.data.slip.advice;
-    adviceId.innerHTML = response.data.slip.id;
-  })
-  
-  .catch(e => {
-    console.log("ERROR!:", e.message)
-  })
+addEventListener("load", (event) => {
+updateData();
 });
+
+button.addEventListener("click", (event) => {
+  updateData()
+});
+
+function updateData() {
+  const getData = fetch("https://api.adviceslip.com/advice", { cache: "no-store" })
+.then((res) => {
+  return res.json();
+})
+.then((data) => {
+  console.log(data.slip);
+  adviceText.innerText = data.slip.advice;
+  adviceId.innerText = data.slip.id;
+})
+.catch((e) => {
+  console.log("error!", e)
+});
+}
+
+
+
 
